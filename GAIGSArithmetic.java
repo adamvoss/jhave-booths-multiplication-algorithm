d686b7dc885e0566f96805fa1e83bf8f36d4e41e
Adam Voss <vossad01@gmail.com>
2011-06-24 19:58:45 -0500

    can now color arbitrary bits

--- GAIGSArithmetic.java
+++ GAIGSArithmetic.java
@@ -7,24 +7,33 @@ import exe.GAIGSdatastr;
 import exe.ShowFile;
 import exe.boothsMultiplication.GAIGSmonospacedText;
 /**
+ * <code>GAIGSArithmetic</code> provides the ability to visualize arithmetic at various
+ * stages of execution, in any based supported by the Character Class.
+ * 
+ * Currently only addition of two terms is fully implemented.
  * 
  * @author Adam Voss <vossad01@luther.edu>
  *
  */
+//TODO color support could be added by keeping an Color ArrayList
 public class GAIGSArithmetic implements GAIGSdatastr {
 	private static final boolean DEBUG = false;
+	private static final double DEFAULT_FONT_SIZE = .05;
+	private static final String DEFAULT_COLOR = "#000000";
 	private String name = "";
-//	private ArrayList<ArrayList<Character>> terms = new ArrayList<ArrayList<Character>>();
+	//	private ArrayList<ArrayList<Character>> terms = new ArrayList<ArrayList<Character>>();
 	private ArrayList<char[]> terms = new ArrayList<char[]>();
+	private ArrayList<String[]> colors = new ArrayList<String[]>();
 	private int firstTermIndex;
 	private int lastTermIndex;
 	private char op;
-	private int currentDigit = 0;
-	private int maxLength = 0;
+	private int currentDigit;
+	private int maxLength;
 	private double xright;
 	private double ytop;
 	private int radix;
 	
+
 	/**
 	 * Construct a new Arithmetic object. It is capable of performing displaying
 	 * math in any radix supported by the Character class.
@@ -40,11 +49,10 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 	public GAIGSArithmetic(char op, String term1, String term2, int radix, double x0, double y0){
 		firstTermIndex = 0;
 		this.radix = radix;
-		
-		
+
 		xright=x0;
 		ytop=y0;
-		
+
 		maxLength = 0;
 		int t1len = term1.length();
 		int t2len = term2.length();
@@ -53,7 +61,7 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 		} else{
 			maxLength = t2len+1;
 		}
-		
+
 		switch (op){
 		case '*':
 			this.op='×'; break;
@@ -67,8 +75,14 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 		lastTermIndex = terms.size()-1;
 		terms.add(emptyRow());
 		addCarryRow();
+		initializeColorArray();
+		this.colors.get(0)[7]="#FF0000";
 	}
-	
+
+	/**
+	 * Returns an empty row of maxLength filled with spaces characters.
+	 * @return
+	 */
 	private char[] emptyRow(){
 		char[] ret = new char[maxLength];
 		for (int i = 0; i < maxLength; i++){
@@ -76,7 +90,18 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 		}
 		return ret;
 	}
-
+	
+	private void initializeColorArray(){
+		for (int i = 0; i < terms.size(); i++){
+			colors.add(new String[terms.get(i).length]);
+		}
+	}
+	
+	/**
+	 * Reverses the given array by modifying its entries.
+	 * @param c A char array
+	 * @return The array that was passed in
+	 */
 	private char[] inplaceReverse(char[] c){
 		for(int i = 0, j = c.length - 1; i < j; i++, j--){
 			char temp = c[j];
@@ -86,21 +111,26 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 		return c;
 	}
 
-	//TODO make this more efficient (start with an empty array rather than clone)
-	private char[] reverse(char[] c){
-		return inplaceReverse(c.clone());
-	}
-	
+	/**
+	 * Adds a new row for carry operations and adjusts Indexes accordingly.
+	 */
 	public void addCarryRow(){
 		terms.add(0,emptyRow());
 		firstTermIndex++;
 		lastTermIndex++;
 	}
-	
+
+	/**
+	 * Returns true if the arithmetic has not yet completed.
+	 * @return true if there are more steps, otherwise false.
+	 */
 	public boolean hasStep(){
 		return (currentDigit < maxLength);
 	}
-	
+
+	/**
+	 * Execute one step of the arithmetic.
+	 */
 	public void step(){
 		if (currentDigit < maxLength){
 			switch (op){
@@ -109,18 +139,25 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 			}
 		}
 	}
-	
+
+	/**
+	 * Executes all remaining steps of the arithmetic.
+	 */
 	public void complete(){
 		while (this.hasStep()){
 			this.step();
 		}
 	}
-	
+
+	/**
+	 * step() function for addition
+	 * @see step()
+	 */
 	private void additionStep() {
 		int sum = 0;
 		for (int i = 0 ; i <= lastTermIndex; i++){
 			char[] nextLine = terms.get(i);
-			
+
 			if (nextLine.length > currentDigit){
 				char nextChar = nextLine[currentDigit];
 				int next = Character.digit(nextChar, radix);
@@ -140,48 +177,62 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 		}
 		//Set result
 		terms.get(lastTermIndex+1)[currentDigit]=Character.toUpperCase(Character.forDigit(sum % radix, radix));
-		
+
 		currentDigit++;
 	}
 
 
+	/**
+	 * Provides GAIGS-compliant XML describing the state for the arithmetic.
+	 */
 	@Override
 	// TODO Consider aligning print based on first term.
 	public String toXML() {
 		String ret = "<!-- Start of GAIGSArithmetic -->\n";
 
-		String print="";
+		String print = "";
 		int i = 0;
-		
-		//Print Carrys
-		while (i < firstTermIndex){
-			print += new String(reverse(terms.get(i++))) + "\n";
+
+		while (i < terms.size()) {
+			char[] currentTerm = terms.get(i);
+			String[] currentColors = colors.get(i);
+			if (i == lastTermIndex) print += op+ " ";
+			for (int n = currentTerm.length-1 ; n >= 0  ; n-- ){
+				if (currentColors[n] != null){
+					print += "\\"+currentColors[n]+currentTerm[n]+"\\"+DEFAULT_COLOR; 
+				}
+				else{
+					print += currentTerm[n];
+				}
+			}
+			print += "\n";
+			i++;
 		}
-		
-		//All but last term
-		while (i < lastTermIndex){
-			print += new String(reverse(terms.get(i++))) + "\n";
-		}
-		
-		//Print operator with last term
-		print += this.op + " " + new String(reverse(terms.get(i++))) + "\n";
-		
-		//Print the result
-		print += new String(reverse(terms.get(i++))) + "\n";
-		
-		//Create the Text Object
-		ret+= (new GAIGSmonospacedText(xright, ytop, print, GAIGSmonospacedText.HRIGHT, GAIGSmonospacedText.VTOP)).toXML();
-		
-		ret += new GAIGSline(new double[] {xright-(maxLength+1)*GAIGSmonospacedText.DEFAULT_FONT_SIZE, xright}, new double[] {ytop-(lastTermIndex+1)*GAIGSmonospacedText.DEFAULT_FONT_SIZE, ytop-(lastTermIndex+1)*GAIGSmonospacedText.DEFAULT_FONT_SIZE}).toXML();
-		
-		return  ret	+ "\n<!-- End of GAIGS Arithmetic -->\n";
+
+		ret += (new GAIGSmonospacedText(xright, ytop, print,
+				GAIGSmonospacedText.HRIGHT, GAIGSmonospacedText.VTOP)).toXML();
+
+		ret += new GAIGSline(new double[] {
+				xright - (maxLength + 1) * DEFAULT_FONT_SIZE, xright },
+				new double[] { ytop - (lastTermIndex + 1) * DEFAULT_FONT_SIZE,
+				ytop - (lastTermIndex + 1) * DEFAULT_FONT_SIZE })
+		.toXML();
+
+		return ret + "\n<!-- End of GAIGS Arithmetic -->\n";
 	}
 
+	/**
+	 * The mandatory name for the GAIGSdatastr.  Note:  This will never appear on-screen.
+	 */
 	@Override
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Sets the mandatory name for the GAIGSdatastr.  Note:  This will never appear on-screen.
+	 * Defaults to an empty string.
+	 */
 	@Override
 	public void setName(String name) {
 		this.name = name;
@@ -192,17 +243,16 @@ public class GAIGSArithmetic implements GAIGSdatastr {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		
+
 		GAIGSArithmetic test = new GAIGSArithmetic('+', "ZBCTY0Z", "1808051", 36, .5, .5);
 		ShowFile show = new ShowFile("bar.sho");
 		show.writeSnap("Addition", test);
-		
+
 		while (test.hasStep()){
 			test.step();
 			show.writeSnap("Addition", test);
 		}
-		
+
 		show.close();
 	}
-
 }
