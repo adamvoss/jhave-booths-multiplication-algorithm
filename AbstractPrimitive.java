aeb557982f930704096a913872fb71d1b5608a6e
Adam Voss <vossad01@gmail.com>
2011-07-15 17:02:48 -0500

    added javadoc comments to GAIGSprimitiveRegister

--- AbstractPrimitive.java
+++ AbstractPrimitive.java
@@ -15,228 +15,228 @@ import exe.boothsMultiplication.MutableGAIGSdatastr;
  * @author Adam Voss <vossad01@luther.edu>
  */
 public abstract class AbstractPrimitive implements MutableGAIGSdatastr  {
-	private enum TextResizeMode{NONE}
-	
-	/**
-	 * Enum to be used with setTextResizeMode to indicate that no resizing should be done
-	 */
-	public static final TextResizeMode NONE = TextResizeMode.NONE;
-	
-	/**
-	 * The text String that will be drawn with this primitive
-	 */
-	protected String label;
-	
-	/**
-	 * The default fontSize used by primitives
-	 */
-	final public static double TEXT_HEIGHT = .03;
-	
-	/**
-	 * The default lineWidth or Outline Thickness used by primitives
-	 */
-	final public static int LINE_WIDTH = 12;
-	
-	protected String fcolor;
-	protected String ocolor;
-	protected String lcolor;
-	
-	/**
-	 * This is the name field that exists for compliance with the GAIGSdatastr.
-	 * It will appear in the XML, but never on-screen.  For on-screen with primitives
-	 * use their label. 
-	 */
-	protected String name = "";
-	
-	
-	protected double fontSize;
-	protected int lineWidth;
-	protected TextResizeMode text_resize;
-	
-	
-	/**
-	 * Returns the XML that is to be used within <primitivecollection> tags
-	 * for use in GAIGSprimitiveCollection.
-	 * @return String representation of XML elements.
-	 */
-	abstract protected String toCollectionXML();
-	
-	
-	/** 
-	 * Returns a deep-copy of this object.
-	 */
-	abstract public AbstractPrimitive clone();
-	
-	/* (non-Javadoc)
-	 * @see exe.GAIGSdatastr#toXML()
-	 * 
-	 * This is final to discourage subclasses of existing primitives from
-	 * straying far from their roots in GAIGSprimitveCollection.  If you have
-	 * a good reason to extend then remove the final.  Otherwise the recommendation
-	 * it so use the generic GAIGScollection in favor of GAIGSprimitiveCollection.
-	 * 
-	 * Inheritors of Abstract Primitives must provide the
-	 * toCollection XML for use within a collection.
-	 * This allows them to be drawn singly by using a collection
-	 * with only one element. 
-	 */
-	@Override
-	final public String toXML() {
-		return "<primitivecollection>\n\t<name>" + name +
-		"</name>\n\t"+ this.computeBounds() + "\n\t" + 
-		this.toCollectionXML() +"</primitivecollection>\n";
-	}
+    private enum TextResizeMode{NONE}
 
-	
-	
-	/**
-	 * This returns a string containing the &lt;bounds /&gt; XML element  
-	 * @return String representing bounds XML element.
-	 */
-	/* Due to a client side bug/feature the coordinates of cannot be the coordinates of the bounds.
-	 * Instead the coordinates given are drawn relative to the bounds given, wherein a unit square
-	 * primitive perfectly fills bounds on screen.
-	 */
-//	protected String computeBounds() {
-//	double[] bounds = this.getBounds();
-//	return "<bounds x1=\"" + bounds[0] + "\" y1=\"" + bounds[1] + "\" x2=\"" + bounds[2] + "\" y2=\"" + bounds[3] + "\"/>";
-//}
-	//This is final for the same reason as toXML()
-	final protected String computeBounds() {
-		return "<bounds x1=\"0.0\" y1=\"0.0\" x2=\"1.0\" y2=\"1.0\"/>";
-	}
+    /**
+     * Enum to be used with setTextResizeMode to indicate that no resizing should be done
+     */
+    public static final TextResizeMode NONE = TextResizeMode.NONE;
 
-	/* (non-Javadoc)
-	 * @see exe.GAIGSdatastr#getName()
-	 */
-	@Override
-	public String getName() {
-		return this.name;
-	}
-	/* (non-Javadoc)
-	 * @see exe.GAIGSdatastr#setName(java.lang.String)
-	 */
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * Returns the Fill Color
-	 * @return GAIGS-formated hex color string.
-	 */
-	public String getColor() {
-		return fcolor;
-	}
-	
-	/**
-	 * Sets the fill color of the primitive to the given String.
-	 * null results in Black; however, the empty string "" will result in no fill or transparency.
-	 * @param fcolor GAIGS-formated hex color string.
-	 */
-	public void setColor(String fcolor) {
-		this.fcolor = fcolor;
-	}
-	
-	/**
-	 * Returns the outline color of the primitive.
-	 * @return GAIGS-formated hex color string.
-	 */
-	public String getOutlineColor() {
-		return ocolor;
-	}
-	
-	/**
-	 * Sets the outline color of the primitive to the given String. 
-	 * @param ocolor GAIGS-formated hex color string.
-	 */
-	public void setOutlineColor(String ocolor) {
-		this.ocolor = ocolor;
-	}
-	/**
-	 * Sets the label, or text color of the primitive.
-	 * @return GAIGS-formated hex color string.
-	 */
-	public String getLabelColor() {
-		return lcolor;
-	}
-	
-	/**
-	 * Returns the label, or text color of the primitive to the given String.
-	 * @param lcolor GAIGS-formated hex color string.
-	 */
-	public void setLabelColor(String lcolor) {
-		this.lcolor = lcolor;
-	}
-	
-	/**
-	 * The thickness of the outline.
-	 * @return int value of with outline thickness.
-	 */
-	public int getLineWidth() {
-		return lineWidth;
-	}
-	
-	/**
-	 * Sets the outline thickness to the given value
-	 * @param lineWidth int value of the desired thickness.
-	 */
-	public void setLineWidth(int lineWidth) {
-		this.lineWidth = lineWidth;
-	}
-	
-	/**
-	 * Returns the label text.
-	 * @return String currently used as the label
-	 */
-	public String getLabel() {
-		return this.label;
-	}
-	
-	/**
-	 * Sets the label text which will be displayed with the object.
-	 * Use the empty string is you want no text to display.
-	 * @param label String desired for display.
-	 */
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	
-	/**
-	 * Returns the value of the font size.
-	 * The scale is based on the unit lengt of the JHAVÉ.
-	 * i.e. A font size of 1 would create text with height that would
-	 * span the unit square.  Therefore, 0.07 size font would have a height
-	 * of 7% of the same unit square. 
-	 * @return  The current font size.
-	 */
-	@Override
-	public double getFontSize(){
-		return this.fontSize;
-	}
+    /**
+     * The text String that will be drawn with this primitive
+     */
+    protected String label;
 
-	/**
-	 * Sets the font size for display.
-	 * @param      fontSize     The desired font size.
-	 */
-	@Override
-	public void setFontSize(double fontSize){
-		this.fontSize = fontSize;
-	}
-	
-	/**
-	 * Returns the enum that corresponds to how fontSize should change if the object is resized.
-	 * @return enum of the current mode.
-	 */
-	public TextResizeMode getTextResizeMode(){
-		return this.text_resize;
-	}
-	
-	/**
-	 * Sets how text should resize when the object is scaled.
-	 * @param mode Enum of the desired mode.
-	 */
-	public void setTextResizeMode(TextResizeMode mode){
-		this.text_resize = mode;
-	}
+    /**
+     * The default fontSize used by primitives
+     */
+    final public static double TEXT_HEIGHT = .03;
+
+    /**
+     * The default lineWidth or Outline Thickness used by primitives
+     */
+    final public static int LINE_WIDTH = 12;
+
+    protected String fcolor;
+    protected String ocolor;
+    protected String lcolor;
+
+    /**
+     * This is the name field that exists for compliance with the GAIGSdatastr.
+     * It will appear in the XML, but never on-screen.  For on-screen with primitives
+     * use their label. 
+     */
+    protected String name = "";
+
+
+    protected double fontSize;
+    protected int lineWidth;
+    protected TextResizeMode text_resize;
+
+
+    /**
+     * Returns the XML that is to be used within <primitivecollection> tags
+     * for use in GAIGSprimitiveCollection.
+     * @return String representation of XML elements.
+     */
+    abstract protected String toCollectionXML();
+
+
+    /** 
+     * Returns a deep-copy of this object.
+     */
+    abstract public AbstractPrimitive clone();
+
+    /* (non-Javadoc)
+     * @see exe.GAIGSdatastr#toXML()
+     * 
+     * This is final to discourage subclasses of existing primitives from
+     * straying far from their roots in GAIGSprimitveCollection.  If you have
+     * a good reason to extend then remove the final.  Otherwise the recommendation
+     * it so use the generic GAIGScollection in favor of GAIGSprimitiveCollection.
+     * 
+     * Inheritors of Abstract Primitives must provide the
+     * toCollection XML for use within a collection.
+     * This allows them to be drawn singly by using a collection
+     * with only one element. 
+     */
+    @Override
+    final public String toXML() {
+        return "<primitivecollection>\n\t<name>" + name +
+        "</name>\n\t"+ this.computeBounds() + "\n\t" + 
+        this.toCollectionXML() +"</primitivecollection>\n";
+    }
+
+
+
+    /**
+     * This returns a string containing the &lt;bounds /&gt; XML element  
+     * @return String representing bounds XML element.
+     */
+    /* Due to a client side bug/feature the coordinates of cannot be the coordinates of the bounds.
+     * Instead the coordinates given are drawn relative to the bounds given, wherein a unit square
+     * primitive perfectly fills bounds on screen.
+     */
+    //	protected String computeBounds() {
+    //	double[] bounds = this.getBounds();
+    //	return "<bounds x1=\"" + bounds[0] + "\" y1=\"" + bounds[1] + "\" x2=\"" + bounds[2] + "\" y2=\"" + bounds[3] + "\"/>";
+    //}
+    //This is final for the same reason as toXML()
+    final protected String computeBounds() {
+        return "<bounds x1=\"0.0\" y1=\"0.0\" x2=\"1.0\" y2=\"1.0\"/>";
+    }
+
+    /* (non-Javadoc)
+     * @see exe.GAIGSdatastr#getName()
+     */
+    @Override
+    public String getName() {
+        return this.name;
+    }
+    /* (non-Javadoc)
+     * @see exe.GAIGSdatastr#setName(java.lang.String)
+     */
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the Fill Color
+     * @return GAIGS-formated hex color string.
+     */
+    public String getColor() {
+        return fcolor;
+    }
+
+    /**
+     * Sets the fill color of the primitive to the given String.
+     * null results in Black; however, the empty string "" will result in no fill or transparency.
+     * @param fcolor GAIGS-formated hex color string.
+     */
+    public void setColor(String fcolor) {
+        this.fcolor = fcolor;
+    }
+
+    /**
+     * Returns the outline color of the primitive.
+     * @return GAIGS-formated hex color string.
+     */
+    public String getOutlineColor() {
+        return ocolor;
+    }
+
+    /**
+     * Sets the outline color of the primitive to the given String. 
+     * @param ocolor GAIGS-formated hex color string.
+     */
+    public void setOutlineColor(String ocolor) {
+        this.ocolor = ocolor;
+    }
+    /**
+     * Sets the label, or text color of the primitive.
+     * @return GAIGS-formated hex color string.
+     */
+    public String getLabelColor() {
+        return lcolor;
+    }
+
+    /**
+     * Returns the label, or text color of the primitive to the given String.
+     * @param lcolor GAIGS-formated hex color string.
+     */
+    public void setLabelColor(String lcolor) {
+        this.lcolor = lcolor;
+    }
+
+    /**
+     * The thickness of the outline.
+     * @return int value of with outline thickness.
+     */
+    public int getLineWidth() {
+        return lineWidth;
+    }
+
+    /**
+     * Sets the outline thickness to the given value
+     * @param lineWidth int value of the desired thickness.
+     */
+    public void setLineWidth(int lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
+    /**
+     * Returns the label text.
+     * @return String currently used as the label
+     */
+    public String getLabel() {
+        return this.label;
+    }
+
+    /**
+     * Sets the label text which will be displayed with the object.
+     * Use the empty string is you want no text to display.
+     * @param label String desired for display.
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * Returns the value of the font size.
+     * The scale is based on the unit lengt of the JHAVÉ.
+     * i.e. A font size of 1 would create text with height that would
+     * span the unit square.  Therefore, 0.07 size font would have a height
+     * of 7% of the same unit square. 
+     * @return  The current font size.
+     */
+    @Override
+    public double getFontSize(){
+        return this.fontSize;
+    }
+
+    /**
+     * Sets the font size for display.
+     * @param      fontSize     The desired font size.
+     */
+    @Override
+    public void setFontSize(double fontSize){
+        this.fontSize = fontSize;
+    }
+
+    /**
+     * Returns the enum that corresponds to how fontSize should change if the object is resized.
+     * @return enum of the current mode.
+     */
+    public TextResizeMode getTextResizeMode(){
+        return this.text_resize;
+    }
+
+    /**
+     * Sets how text should resize when the object is scaled.
+     * @param mode Enum of the desired mode.
+     */
+    public void setTextResizeMode(TextResizeMode mode){
+        this.text_resize = mode;
+    }
 }
--- CountBox.java
+++ CountBox.java
@@ -3,12 +3,22 @@ package exe.boothsMultiplication;
 //THIS IS A VERY UGLY HACK
 //This Should Never Have Used Inheritance Like This
 //I Have Lost So Much Time To Maintaining This
+/**
+ * THIS IS NOT A GENERAL USE CLASS.
+ * It was designed specifically for use in the
+ * Booths Multiplication Algorithm Visualization.
+ * 
+ * It is a bad example of inheritance. 
+ * 
+ * @author Chris Jenkins <cjenkin1@trinity.edu>
+ * @author Adam Voss <vossad01@luther.edu>
+ */
 public class CountBox extends GAIGSprimitiveRegister {
     public CountBox(int count, String color, String fontColor, String outlineColor, double x1, double y1, double x2, double y2, double fontSize) {
         super(String.valueOf(count).length(), "", color, fontColor, outlineColor, x1, y1, x2, y2, fontSize);
         this.set(String.valueOf(count));
     }
-    
+
     public CountBox(int count, String color, String fontColor, String outlineColor, double[] bounds, double fontSize) {
         this(count, color, fontColor, outlineColor, bounds[0], bounds[1], bounds[2], bounds[3], fontSize);
     }
@@ -18,25 +28,25 @@ public class CountBox extends GAIGSprimitiveRegister {
     }
 
     public void decrement() {
-    	this.set(String.valueOf(getCount()-1));
-    	}
+        this.set(String.valueOf(getCount()-1));
+    }
 
     public int getCount()   {return new Integer(this.toString());}
-    
+
     @Override
     public CountBox clone() {
-    	return new CountBox(this);
+        return new CountBox(this);
     }
-    
+
     /**
-     * This exists to have set always pad with 0s
+     * This exists to have set always pad with 0s.
      * It is untested
      */
     @Override
     public String signExtend(String binStr, int i){
-    	String firstBit = "0";
-    	String extension = "";
-    	while (i>0){extension = extension.concat(firstBit); i--;}
-    	return extension.concat(binStr);
+        String firstBit = "0";
+        String extension = "";
+        while (i>0){extension = extension.concat(firstBit); i--;}
+        return extension.concat(binStr);
     }
 }
