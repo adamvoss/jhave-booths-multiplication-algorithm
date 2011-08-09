9dc2cfac17d51d40f3007aebb6273220c9b21dbf
Chris Jenkins <cwjnesa@gmail.com>
2011-08-08 22:57:57 -0500

    Exercise 1 input generator sane working, logic in boothsMultiplication to handle multiple visualizations. Input generator still needs cleanup

--- boothsMultiplication.java
+++ boothsMultiplication.java
@@ -14,17 +14,36 @@ public class boothsMultiplication{
 
     public static void main(String[] args)throws IOException, JDOMException
     {
-    @SuppressWarnings("unchecked")
-    Hashtable<String, String> param = XMLParameterParser.parseToHash(args[2]);
+        @SuppressWarnings("unchecked")
+        Hashtable<String, String> param = XMLParameterParser.parseToHash(args[2]);
 
-        String[] toPass = new String[3];
+        if (param.containsKey("Booth's Multiplication Input") ) {
+            String[] toPass = new String[3];
+            toPass[0] = args[0] + ".sho";
+            String[] temp = param.get("Booth's Multiplication Input").split(" ");
+            toPass[1] = temp[0];
+            toPass[2] = temp[1];
+
+            BoothMultiplication.main(toPass);
+        }
+        else if (param.containsKey("Exercise 1 for Booth's Multiplication") ) {
+            System.out.println("Made it here");
+        }
+        else if (param.containsKey("Exercise 2 for Booth's Multiplication") ) {
+        }
+        else if (param.containsKey("Exercise 3 for Booth's Multiplication") ) {
+        }
+        else {
+        }
+
+/*      String[] toPass = new String[3];
         toPass[0] = args[0] + ".sho";
         String[] temp = param.get("Booth's Multiplication Input").split(" ");
         toPass[1] = temp[0];
         toPass[2] = temp[1];
 
         BoothMultiplication.main(toPass);
-
+*/
 //      BoothsMultiplication.main(new String[] {args[0] + ".sho",
 //                                             param.get("Multiplicand"),
 //                                             param.get("Multiplier")});
