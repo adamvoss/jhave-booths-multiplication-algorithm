b3a04b6412049952f101eaf5e25f4e812712968f
Adam Voss <vossad01@gmail.com>
2011-10-01 09:37:17 -0500

    added previously untracked research mind map

---
+++ researchNotes.mm
@@ -0,0 +1,129 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1316486930312" ID="ID_1911761146" MODIFIED="1317479748052" TEXT="Research">
+<node CREATED="1316651482310" FOLDED="true" ID="ID_1556935267" MODIFIED="1316651773569" POSITION="right" TEXT="CALO">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Integrating Categories of Algorithm Learning Objective
+    </p>
+    <p>
+      into Algorithm Visualization Design: a Proposal
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1316487086013" ID="ID_425456642" MODIFIED="1316570178482" TEXT="Coding: Fill in missing line of pseudocode"/>
+<node CREATED="1316487164144" ID="ID_959264481" MODIFIED="1316570168748" TEXT="Demonstrative: Predminantly demonstrative">
+<node CREATED="1316570219401" ID="ID_285273311" MODIFIED="1316570255949" TEXT="Give unlabled &apos;register pictures&apos; and have them label, identify point in algorithm"/>
+</node>
+<node CREATED="1316570020651" ID="ID_1434757162" MODIFIED="1316570052915" TEXT="Descriptive: Try to trick them with another algorithm"/>
+<node CREATED="1316570088061" ID="ID_1737900854" MODIFIED="1316570174999" TEXT="Decoding: Parson&apos;s problem, arrange pseudocode"/>
+</node>
+<node CREATED="1316651552690" FOLDED="true" ID="ID_1689174888" MODIFIED="1316653662947" POSITION="left" TEXT="Tom&apos;s Multi-Institutional Study">
+<node CREATED="1316651579954" ID="ID_1449338639" MODIFIED="1316651692894" TEXT="They purpously excluded all graphics outside AV">
+<node CREATED="1316651697358" ID="ID_986951411" MODIFIED="1316651699729" TEXT="not a fair comparison with your average textbook"/>
+</node>
+<node CREATED="1316651602611" ID="ID_233152645" MODIFIED="1316651719825" TEXT="They supposed it was notably advantagous to the student that the visualization matched the other materials">
+<node CREATED="1316651678809" ID="ID_912041213" MODIFIED="1316651681008" TEXT="Maybe could study not having that similarity"/>
+</node>
+<node CREATED="1316653618582" ID="ID_1433116354" MODIFIED="1316653640045" TEXT="Didn&apos;t indicate how affected grades, need to remember this is a factor">
+<node CREATED="1316653641568" ID="ID_1502679632" MODIFIED="1316653659191" TEXT="If is a greater portion of the grade students are likely to spend more time on it"/>
+</node>
+</node>
+<node CREATED="1316651876956" FOLDED="true" ID="ID_711454284" MODIFIED="1317479718966" POSITION="right" TEXT="Meta-Study">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A Meta-Study of AlgorithmVisualization E&#161;ectiveness
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      CHRISTOPHER D. HUNDHAUSEN, SARAH A. DOUGLAS
+    </p>
+    <p>
+      ANDJOHN T. STASKO
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1316651900450" ID="ID_1723199500" MODIFIED="1316651936971" TEXT="Their &apos;test&apos; usage is a different kind of visulization">
+<node CREATED="1316651938751" ID="ID_195836257" MODIFIED="1316651949955" TEXT="happens more frequently with out it ever being called such"/>
+</node>
+<node CREATED="1316652123600" ID="ID_1035153073" MODIFIED="1316652129012" TEXT="Taxonomy">
+<node CREATED="1316652129015" ID="ID_1571566948" MODIFIED="1316978345363" TEXT="Anecdotal">
+<node CREATED="1316652305330" ID="ID_1676393745" MODIFIED="1316652321207" TEXT="What makes it better for X situation"/>
+</node>
+<node CREATED="1316652170297" ID="ID_1067445583" MODIFIED="1316654236500" TEXT="Programatic">
+<node CREATED="1316652200215" ID="ID_571228057" MODIFIED="1316652256939" TEXT="# lines for us to produce visualization high == bad"/>
+</node>
+<node CREATED="1316652180559" ID="ID_1258443631" MODIFIED="1316978168434" TEXT="Analytic">
+<node CREATED="1316652374075" ID="ID_232238362" MODIFIED="1316652396584" TEXT="testing boil down to user-perception, usability">
+<node CREATED="1316652461592" ID="ID_1601431894" MODIFIED="1316654505613" TEXT="&quot;the fewer usability problems identified, the more effective the system&quot;"/>
+</node>
+</node>
+<node CREATED="1316652186442" ID="ID_943510922" MODIFIED="1316653081691" TEXT="Empirical">
+<node CREATED="1316652757370" ID="ID_1644312186" MODIFIED="1316652766376" TEXT="Actual data from humans"/>
+<node CREATED="1316652768197" ID="ID_35252756" MODIFIED="1316652782378" TEXT="Tries to solve a research question"/>
+<node CREATED="1316653081692" ID="ID_1750976708" MODIFIED="1316654334355" TEXT="Various findings (each have on source too)">
+<node CREATED="1316653100531" ID="ID_318639157" MODIFIED="1316653232180" TEXT="Custom input, signficantly better performance than supplied input, or no animation "/>
+<node CREATED="1316653240668" ID="ID_137542321" MODIFIED="1316653255118" TEXT="Concret leaners benefited more than abstract learners"/>
+<node CREATED="1316653295013" ID="ID_725315977" MODIFIED="1316653320384" TEXT="Hansen did a lot of studies">
+<node CREATED="1316653717752" ID="ID_961723609" MODIFIED="1316654329978" TEXT="HalVis looks worth looking into">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It has a lot of studies, most successful
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1316653452122" ID="ID_1595257417" MODIFIED="1316653469200" TEXT="Read Stasko to get an idea of statistics, group size needed"/>
+</node>
+</node>
+</node>
+<node CREATED="1316655757378" ID="ID_1362292412" MODIFIED="1317478802956" TEXT="Cognative Constructivism">
+<node CREATED="1316655793614" ID="ID_1018475646" MODIFIED="1316962501507" TEXT="&quot;Applying this reasoning to controlled experimental comparisons, we find that Cognitive Constructivism predicts an educational advantage only in cases in which the activity of one treatment group is more effortful than that of the other treatment group(s).&quot;">
+<node CREATED="1316656285996" ID="ID_175819558" MODIFIED="1316656299764" TEXT="May be important to try to control/normalize this"/>
+</node>
+</node>
+<node CREATED="1317478807980" FOLDED="true" ID="ID_182466465" MODIFIED="1317478877521" TEXT="Other theories besides Cognative Constructivism">
+<node CREATED="1317478822936" ID="ID_1468999026" MODIFIED="1317478836610" TEXT="Epistemic Fidelity">
+<node CREATED="1317478836613" ID="ID_1471677358" MODIFIED="1317478846636" TEXT="Doesn&apos;t seem very effective"/>
+</node>
+<node CREATED="1317478849012" ID="ID_1302500653" MODIFIED="1317478853143" TEXT="Dual-coding">
+<node CREATED="1317478854080" ID="ID_503019250" MODIFIED="1317478860111" TEXT="Few studies done"/>
+</node>
+<node CREATED="1317478860982" ID="ID_1003631675" MODIFIED="1317478870525" TEXT="Indivudial Differences">
+<node CREATED="1317478871201" ID="ID_360609406" MODIFIED="1317478875654" TEXT="Few studies done"/>
+</node>
+</node>
+<node CREATED="1317477901621" ID="ID_648172305" MODIFIED="1317478191197" TEXT="Studies test one of two distinct types of knowledge">
+<node CREATED="1317477922832" ID="ID_1817352488" MODIFIED="1317477931748" TEXT="conceptual or declarative">
+<node CREATED="1317477932732" ID="ID_1881287045" MODIFIED="1317477940733" TEXT="Abstract properties of the algorithm">
+<node CREATED="1317477940736" ID="ID_579103266" MODIFIED="1317477944399" TEXT="ex. efficency"/>
+</node>
+</node>
+<node CREATED="1317477957395" ID="ID_817562710" MODIFIED="1317477966127" TEXT="procedural">
+<node CREATED="1317477966131" ID="ID_1810121926" MODIFIED="1317477979310" TEXT="the step-by-step behavior of the algorithm"/>
+</node>
+<node CREATED="1317478194780" ID="ID_1683465163" MODIFIED="1317478229679" TEXT="study measures that studied one or the other were more likely to show significance than those that studied both"/>
+<node CREATED="1317478270334" ID="ID_739540438" MODIFIED="1317478289471" TEXT="procedural seems to be more easily thought with visualization"/>
+</node>
+<node CREATED="1317477981230" ID="ID_1210475295" MODIFIED="1317478410601" TEXT="Studies that used a pre-test, post-test were more likely to show significance than those using post-test only"/>
+<node CREATED="1317478905068" ID="ID_1785507788" MODIFIED="1317479149674" TEXT="&quot;Thus, according to our analysis, how students use AV technology, rather than what students see, appears to have the greatest impact on educational effectiveness.&quot;"/>
+<node CREATED="1317479151938" ID="ID_689314087" MODIFIED="1317479194917" TEXT="Result of effectiveness is determined by level of engagement (see Conclusion 6.3)"/>
+</node>
+</node>
+</map>
